24feb85fd700bf4ab54768f91b4ad0b81bb2b0b5
atahtee <atatisam14@gmail.com>
2025-03-18 12:57:37 +0300

    Modifies server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require("cors")
 const connectDB = require("./config/db")
 const letterRoutes = require("./routes/letterRoutes")
 const authRoutes = require("./routes/authRoutes")
+const childProfilesRouter = require("./routes/childRoutes")
 
 dotenv.config();
 connectDB();
@@ -15,6 +16,7 @@ app.use(express.json());
 
 app.use("/auth", authRoutes)
 app.use("/api/letters", letterRoutes);
+app.use("/api/child-profiles", childProfilesRouter)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
