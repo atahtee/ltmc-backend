975e35bf8df33f529dbc89dd376fdfd7c2be6b47
atahtee <atatisam14@gmail.com>
2025-03-26 10:32:38 +0300

    Added post new letter route

--- routes/letterRoutes.js
+++ routes/letterRoutes.js
@@ -2,21 +2,38 @@ const express = require("express")
 const router = express.Router();
 const Letter = require ("../models/letter");
 const verifyToken = require("../middleware/authMiddleware");
+const child = require("../models/child");
 
 router.post("/post-new-letter", verifyToken, async (req, res) => {
     try {
-        const {sender, recipient, message, deliveryDate} = req.body;
-        const newLetter = new Letter({sender, recipient, message, deliveryDate, userId: req.userId});
+        const { sender, recipient, message, deliveryDate } = req.body;
+
+        console.log("Request body:", req.body);
+
+        const childProfile = await child.findOne({ _id: recipient, parent: req.userId });
+        if (!childProfile) {
+            return res.status(404).json({ error: "Child profile not found or does not belong to the user" });
+        }
+
+        const newLetter = new Letter({
+            sender,
+            recipient: childProfile._id,
+            message,
+            deliveryDate,
+            userId: req.userId
+        });
+
         await newLetter.save();
         res.status(201).json(newLetter);
     } catch (error) {
-        res.status(500).json({error: "Internal server error"})
+        console.log("This is the error:", error )
+        res.status(500).json({ error: "Internal service error" });
     }
 });
 
 router.get("/my-letters", verifyToken, async (req, res) => {
     try {
-        const letters = await Letter.find({userId: req.userId});
+        const letters = await Letter.find({userId: req.userId}).populate("recipient", "name birthday");
         res.status(200).json(letters);
     } catch (error) {
         res.status(500).json({error: "Internal server error"})
